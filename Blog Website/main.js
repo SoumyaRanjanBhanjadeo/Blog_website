7c56fc04117bfca302f1787495c9a75bf922af79
SoumyaRanjanBhanjadeo <117259424+SoumyaRanjanBhanjadeo@users.noreply.github.com>
2023-06-29 12:24:39 +0530

    Add files via upload

---
+++ Blog Website/main.js
@@ -0,0 +1,58 @@
+// window.addEventListener("keydown", (event) => {
+//   if(event.ctrlKey && (event.key === "C" || event.key === "c")){
+//       event.preventDefault();
+//   }
+//   if(event.key === "F12"){
+//       event.preventDefault();
+//   }
+// })
+
+// document.addEventListener('contextmenu', function(e){
+//   e.preventDefault();
+// })
+
+
+
+const navItems = document.querySelector(".nav__items");
+const openNavBtn = document.querySelector("#open__nav-btn");
+const closeNavBtn = document.querySelector("#close__nav-btn");
+
+const openNav = () => {
+  navItems.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+const closeNav = () => {
+  navItems.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+openNavBtn.addEventListener("click", openNav);
+closeNavBtn.addEventListener("click", closeNav);
+
+const sidebar = document.querySelector("aside");
+const showSidebarBtn = document.querySelector("#show__sidebar-btn");
+const hideSidebarBtn = document.querySelector("#hide__sidebar-btn");
+
+const showSidebar = () => {
+  sidebar.style.left = "0";
+  sidebar.style.top = "72px";
+  showSidebarBtn.style.display = "none";
+  hideSidebarBtn.style.display = "inline-block";
+};
+
+const hideSidebar = () => {
+  sidebar.style.left = "-100%";
+  sidebar.style.top = "72px";
+  showSidebarBtn.style.display = "inline-block";
+  hideSidebarBtn.style.display = "none";
+};
+
+showSidebarBtn.addEventListener("click", showSidebar);
+hideSidebarBtn.addEventListener("click", hideSidebar);
+
+
+
+
